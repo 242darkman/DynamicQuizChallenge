2de650e759705af50914d793869f7e1ba59143a7
Erica Ndong <ericandong6@gmail.com>
2024-01-28 18:38:02 +0100

    add room and waiting-room

---
+++ packages/office-dynamic-quizz/src/app/room/page.js
@@ -0,0 +1,26 @@
+'use client';
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-24 bg-mainColor flex items-center justify-center flex-col">
+      <div>
+        <h1 className="text-5xl">Que souhaitez-vous faire ?</h1>
+      </div>
+      <div className="flex items-center justify-center gap-10 m-10">
+        <a
+          href=""
+          className="w-1/2 px-4 py-2 text-mainColor bg-white rounded hover:bg-secondColor focus:ring focus:ring-secondColor w-full"
+        >
+          Créer une salle
+        </a>
+        <a
+          href="/waiting-room"
+          className="w-1/2 px-4 py-2 text-mainColor bg-white rounded hover:bg-secondColor focus:ring focus:ring-secondColor w-full"
+          style={{ whiteSpace: 'nowrap' }} >
+         Rejoindre une salle
+        </a>
+      </div>
+    </main>
+  );
+}
