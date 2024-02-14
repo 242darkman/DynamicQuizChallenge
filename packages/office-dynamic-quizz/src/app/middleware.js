01a792f3a1bc15b1c87836f1ff0df0db674839fe
242darkman <53907600+242darkman@users.noreply.github.com>
2024-02-14 09:07:53 +0100

    fix: modification du middleware

--- packages/office-dynamic-quizz/src/app/middleware.js
+++ packages/office-dynamic-quizz/src/app/middleware.js
@@ -4,19 +4,16 @@ import { redirect } from 'next/navigation';
 import { sessionStatus } from '@/app/_utils/session';
 import { useEffect } from 'react';
 
-
 export default function withAuth(WrappedComponent) {
   return function WithAuth(props) {
-    const session = sessionStatus;
+
     useEffect(() => {
+      const session = sessionStatus;
+      
       if (!session) {
         redirect('/auth/signin');
       }
-    }, [session]);
-
-    if (!session) {
-      return null;
-    }
+    }, []);
 
     return <WrappedComponent {...props} />
   }
